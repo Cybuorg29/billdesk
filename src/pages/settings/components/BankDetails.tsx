ce7de97eab93d9f4ee6796b2831e3cf4a4397349
Arc Technologies <118550452+ArctechTechnologies@users.noreply.github.com>
2023-12-27 14:14:40 +0530

    Update BankDetails.tsx

--- src/pages/settings/components/BankDetails.tsx
+++ src/pages/settings/components/BankDetails.tsx
@@ -23,7 +23,7 @@ const BankDetails = ({ bank, handleChange }: Props) => {
         <div className='grid grid-cols-2 gap-2 ' >
           <TextField value={bank.name} label='Benifeciary Name' id='benName' onChange={(e) => handleChange(e.target.id, e.target.value)} ></TextField>
           <TextField value={bank.no} label='Account Number' id='no' onChange={(e) => handleChange(e.target.id, e.target.value)} ></TextField>
-          <TextField value={bank.isfc} label='ISFC Code' id='isfc' onChange={(e) => handleChange(e.target.id, e.target.value)} ></TextField>
+          <TextField value={bank.isfc} label='IFSC Code' id='ifsc' onChange={(e) => handleChange(e.target.id, e.target.value)} ></TextField>
           <TextField value={bank.bank} label='Bank' id='bank' onChange={(e) => handleChange(e.target.id, e.target.value)} ></TextField>
           <TextField value={bank.branch} label='Branch' id='branch' onChange={(e) => handleChange(e.target.id, e.target.value)} ></TextField>
         </div>
