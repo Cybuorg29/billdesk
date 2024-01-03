25d9c32d98a2caca0c022444bada1d63ef13ceba
Arc Technologies <118550452+ArctechTechnologies@users.noreply.github.com>
2024-01-03 13:34:07 +0530

    ...

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -16,8 +16,21 @@ import AccountBalanceWalletRoundedIcon from '@mui/icons-material/AccountBalanceW
 import { useAppDispatch } from "../../store/app/hooks";
 import { deleteToken } from "../../store/features/auth/authSlice";
 import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import AddIcon from "../ui/icons/AddIcon";
 type Props = {};
 
+
+interface Navtab {
+  name: string,
+  link: string
+  icon: any
+}
+
+
+interface detailsTab {
+  name: string,
+  Tabs: Navtab[]
+}
 
 
 const NavBar = (props: Props) => {
@@ -45,14 +58,14 @@ const NavBar = (props: Props) => {
       icon: <AccountBalanceWalletRoundedIcon />
     },
     {
-      name:'Clients',
-      link:'/dashboard/client',
-      icon:<PersonIcon/>
+      name: 'Clients',
+      link: '/dashboard/client',
+      icon: <PersonIcon />
     },
     {
-      name:'supplier',
-      link:'/dashboard/supplier',
-      icon:<PersonIcon/>
+      name: 'supplier',
+      link: '/dashboard/supplier',
+      icon: <PersonIcon />
     },
     {
       name: 'Employees',
@@ -60,18 +73,33 @@ const NavBar = (props: Props) => {
       icon: <PersonIcon />
     },
     {
-      name:'SpreadSheet',
-      link:'/spreadsheet',
-      icon:<FormatListNumberedIcon/>
+      name: 'SpreadSheet',
+      link: '/spreadsheet',
+      icon: <FormatListNumberedIcon />
     },
 
 
   ]
 
+
+  const navTabsWithDetails: detailsTab[] = [
+    {
+      name: 'P.O',
+      Tabs: [
+        {
+          name: 'Create P.O',
+          link: '/client',
+          icon: <AddIcon onclick={() => { }} color="black" key={'asdas'} />
+        }
+      ]
+    }
+
+  ]
+
   const dispatch = useAppDispatch()
-   const navigate = useNavigate()
-    const LogoutIconKey = useId();
-     const tuneIconKey = useId();
+  const navigate = useNavigate()
+  const LogoutIconKey = useId();
+  const tuneIconKey = useId();
   return (
     <>
       <div className="relative h-full bg-navbar text-white " >
@@ -85,7 +113,7 @@ const NavBar = (props: Props) => {
             link="/settings"
             icon={<TuneRoundedIcon key={tuneIconKey} />}
           />
-          <div className="flex text-sm p-2 hover:bg-slate-100/10 gap-2 cursor-pointer" onClick={() => { dispatch(deleteToken()); sessionStorage.removeItem('token'); toast.info('Logout sucessfull') ;navigate('/login') }} >
+          <div className="flex text-sm p-2 hover:bg-slate-100/10 gap-2 cursor-pointer" onClick={() => { dispatch(deleteToken()); sessionStorage.removeItem('token'); toast.info('Logout sucessfull'); navigate('/login') }} >
             <div>{<LogoutRoundedIcon key={LogoutIconKey} />}</div>
             {`Logout`}
           </div>
@@ -99,10 +127,31 @@ const NavBar = (props: Props) => {
             navList.map((index) => {
               return (
                 <>
-                  <NavTab  name={index.name} icon={index.icon} link={index.link} key={index.name}  />
+                  <NavTab name={index.name} icon={index.icon} link={index.link} key={index.name} />
 
                 </>
               )
+            })
+          }
+
+          {
+            navTabsWithDetails.map((index: detailsTab) => {
+              return <>
+                <details className=" duration-500" title={index.name} >
+                  <summary className="hover:bg-[#21262C] rounded-lg p-2 pl-3  cursor-pointer" >{index.name}</summary>
+                  {
+                    index.Tabs.map((item: Navtab) => {
+                      return <>
+                        <p className="p-2 flex  hover:bg-[#21262C] rounded-lg  cursor-pointer" onClick={() => navigate(item.link)}>
+                          <div className="grid place-content-center">{item.icon}</div>
+                          <div className="grid place-content-center">{item.name}</div>
+                        </p>
+                      </>
+                    })
+                  }
+
+                </details>
+              </>
             })
           }
 
--- src/store/actions/connections/set.ts
+++ src/store/actions/connections/set.ts
@@ -8,52 +8,52 @@ import { change } from "../../features/loader/loaderSlice";
 import { userDetailSchema } from "../../../models/userModel";
 
 export async function getConnection() {
-     try{
-                   
-        const {auth,connections,userData} = store.getState();
-        const {isConnection} = connections
-        const {istoken,token} = auth
-        if(!istoken||!token||userData._id===''){
+   try {
+
+      const { auth, connections, userData } = store.getState();
+      const { isConnection } = connections
+      const { istoken, token } = auth
+      if (!istoken || !token || userData._id === '') {
          toast.info('verifing login')
-                 
-         }else{
-            store.dispatch(change())
-            const  {data} = await getConnectionsData(userData._id);
-            console.log(data)
-            const res:responceObj = data;
-            if(res.code===200){
-               sucess(res.package)
-               
-            }else{
-               failure(res.message)
-            }
-            store.dispatch(change());
+
+      } else {
+         store.dispatch(change())
+         const { data } = await getConnectionsData(userData._id);
+         console.log(data)
+         const res: responceObj = data;
+         if (res.code === 200) {
+            sucess(res.package)
+
+         } else {
+            failure(res.message)
          }
-         toast.done('')
-
-     }catch(err:any){
-        console.log(err?.message);
-          toast.error("an error occured please try again")
-          store.dispatch(change());
-           toast.error('')
-
-
-     }
-
-}
-
-
-async function sucess(pkg:any) {
-      const payload:actionPayload={
-         type:'set',
-         data:pkg
+         store.dispatch(change());
       }
-      
-      store.dispatch(setConnections(payload));
+      toast.done('')
+
+   } catch (err: any) {
+      console.log(err?.message);
+      toast.error("an error occured please try again")
+      store.dispatch(change());
+      toast.error('')
+
+
+   }
 
 }
 
-function failure(message:string){
+
+async function sucess(pkg: any) {
+   const payload: actionPayload = {
+      type: 'set',
+      data: pkg
+   }
+
+   store.dispatch(setConnections(payload));
+
+}
+
+function failure(message: string) {
    toast.error('an error occured please try again')
    console.log(message);
 }
