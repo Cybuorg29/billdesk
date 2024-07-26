71ace871bc86c5e00e89d0704024cfdb630b8c91
Aditya Chandel <ADITYACHANDEL.22041582@gehu.ac.in>
2024-07-26 16:32:58 +0530

    Update ProdUrl.ts

--- src/api/Url/ProdUrl.ts
+++ src/api/Url/ProdUrl.ts
@@ -1,4 +1,4 @@
 export const baseUrl = "https://billdesk-backend.vercel.app"; //production
 // export const baseUrl = "http://localhost:5000";  //dev 
-export const v2Url = 'http://localhost:8000'; //dev
-// export const v2Url = 'https://backend-typescript-sandy.vercel.app';   //production
+// export const v2Url = 'http://localhost:8000'; //dev
+export const v2Url = 'https://backend-typescript-sandy.vercel.app';   //production
